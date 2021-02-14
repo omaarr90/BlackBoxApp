fa3e99803f9bf3a4a6b8c7be7cb4024bd4f83b06
Omar Alshammari <omar.alshammary@hotmail.com>
2021-02-15 01:06:31 +0300

    Initial Commit

---
+++ Shared/BlackBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlackBoxApp.swift
+//  Shared
+//
+//  Created by Omar Alshammari on 03/07/1442 AH.
+//
+
+import SwiftUI
+
+@main
+struct BlackBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Omar Alshammari on 03/07/1442 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
